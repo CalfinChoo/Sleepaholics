8cab9bc640766db209879a23462e42d67ec827ee
Jackson <jzou00@stuy.edu>
2020-06-09 16:57:33 -0400

    djslfk

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ import urllib.request, json
 import os
 from os import urandom
 
-from db import init_db
+from db import init_db, addUser, checkUser
 from pprint import pprint
 
 app = Flask(__name__)
@@ -16,28 +16,39 @@ app.secret_key = urandom(32)
 DB_FILE = "info.db"
 init_db(DB_FILE)
 
-
 @app.route("/")
 def root():
-    if ('ign' in session):
+    if "ign" in session:
         redirect(url_for("home"))
     return redirect(url_for("login"))
 
 @app.route("/home")
 def home():
+    print(session)
     return render_template("home.html", ign = session["ign"])
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    print(session)
     if len(request.form) == 0:
         return render_template('login.html')
-    session["ign"] = request.form["username"]
-    return redirect(url_for("home"))
+    result = checkUser(DB_FILE, request.form["username"], request.form["password"])
+    if result:
+        session["ign"] = result
+        return redirect(url_for("home"))
+    return render_template('login.html', errormessage = "invalid credentials")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template('register.html')
+    print(session)
+    if len(request.form) == 0:
+        return render_template('register.html')
+    if (addUser(DB_FILE, request.form["username"], request.form["password"], request.form["password2"], request.form["ign"])):
+        session["ign"] = request.form["ign"]
+        return redirect(url_for("home"))
+    return render_template('register.html', errormessage = "passwords do not match or username is taken")
+
 
 
 if __name__ == "__main__":
--- app/db.py
+++ app/db.py
@@ -1,11 +1,36 @@
 import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
 
 def init_db(DB_FILE):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' ''')
-    if c.fetchone()[0] < 1:
-        c.execute("CREATE TABLE USER(username TEXT, password TEXT, ign TEXT);")
-        c.execute("CREATE TABLE GAMES(room_code TEXT, password TEXT, player_count INTEGER);")
+    c.execute("CREATE TABLE IF NOT EXISTS USER(username TEXT, password TEXT, ign TEXT);")
+    c.execute("CREATE TABLE IF NOT EXISTS GAMES(room_code TEXT, password TEXT, player_count INTEGER);")
     db.commit()
     db.close()
+
+def addUser(DB_FILE, username, password, password2, ign):
+    if password != password2:
+        return False
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("SELECT * FROM USER WHERE username = \"{}\";".format(username))
+    q = c.fetchall()
+
+    if len(q) == 0:
+        c.execute("INSERT INTO USER VALUES(\"{}\", \"{}\", \"{}\");".format(username, generate_password_hash(password), ign))
+        db.commit()
+        db.close()
+        return True
+    return False
+
+def checkUser(DB_FILE, username, password):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("SELECT * FROM USER WHERE username = \"{}\"".format(username))
+    q = c.fetchall()
+    if len(q) == 0:
+        return False
+    if check_password_hash(q[0][1], password):
+        return q[0][2]
+    return False
