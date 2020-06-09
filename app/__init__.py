a89a405f0910d25d5422cf9966b7d643aedcef20
Jackson <jzou00@stuy.edu>
2020-06-09 18:20:33 -0400

    setup all the basics

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,10 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 import urllib.request, json
 import os
+import random
 from os import urandom
 
-from db import init_db, addUser, checkUser
+from db import init_db, addUser, checkUser, createRoom, checkroomid, findRoom
 from pprint import pprint
 
 app = Flask(__name__)
@@ -21,11 +22,6 @@ def root():
     if "ign" in session:
         redirect(url_for("home"))
     return redirect(url_for("login"))
-
-@app.route("/home")
-def home():
-    print(session)
-    return render_template("home.html", ign = session["ign"])
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -49,7 +45,35 @@ def register():
         return redirect(url_for("home"))
     return render_template('register.html', errormessage = "passwords do not match or username is taken")
 
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    print(session)
+    if len(request.form) == 0:
+        return render_template("home.html", ign = session["ign"])
+    if findRoom(DB_FILE, request.form["room_id"], request.form["password"]):
+        return redirect("/game/{}".format(request.form["room_id"]))
+    return render_template("home.html", errormessage = "Room not found or incorrect password")
 
+@app.route("/create")
+def create():
+    set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    room_id = ""
+    dup = True
+    while dup:
+        for i in range(8):
+            room_id += random.choice(set)
+        if checkroomid(DB_FILE, room_id):
+            dup = False
+    return render_template("create.html", room_id = room_id)
+
+@app.route("/initroom/<room_id>", methods=["GET", 'POST'])
+def initroom(room_id):
+    createRoom(DB_FILE, room_id, request.form["password"])
+    return redirect("/game/{}".format(room_id))
+
+@app.route("/game/<room_id>")
+def game(room_id):
+    return render_template("game.html", room_id = room_id)
 
 if __name__ == "__main__":
     app.debug = True
--- app/db.py
+++ app/db.py
@@ -27,10 +27,43 @@ def addUser(DB_FILE, username, password, password2, ign):
 def checkUser(DB_FILE, username, password):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("SELECT * FROM USER WHERE username = \"{}\"".format(username))
+    c.execute("SELECT * FROM USER WHERE username = \"{}\";".format(username))
     q = c.fetchall()
     if len(q) == 0:
         return False
     if check_password_hash(q[0][1], password):
         return q[0][2]
+    return False
+
+def checkroomid(DB_FILE, room_id):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("SELECT * FROM GAMES WHERE room_code = \"{}\";".format(room_id))
+    q = c.fetchall()
+    db.close()
+    if len(q) == 0:
+        return True
+    return False
+
+def createRoom(DB_FILE, room_id, password):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("INSERT INTO GAMES VALUES(\"{}\", \"{}\", 1);".format(room_id, generate_password_hash(password)))
+    db.commit()
+    db.close()
+    return True
+
+def findRoom(DB_FILE, room_id, password):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("SELECT * FROM GAMES WHERE room_code = \"{}\";".format(room_id))
+    q = c.fetchall()
+    if len(q) == 0:
+        return False
+    if check_password_hash(q[0][1], password):
+        playercount = q[0][2] + 1
+        c.execute("UPDATE GAMES SET player_count = {} WHERE room_code = \"{}\"".format(playercount, room_id))
+        db.commit()
+        db.close()
+        return True
     return False
